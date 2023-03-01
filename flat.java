92f45eff34767335ca18a8ca9122f55e77caf85f
0-1994-0 <aurelie.zouaoui@edu.fh-joanneum.at>
2023-03-01 19:43:50 +0100

    voici mes changements

---
+++ flat.java
@@ -0,0 +1,8 @@
+public class flat {
+    public static void main(String[] args) {
+        int hhh = 2;
+        double hhhh = 2.0;
+        double result = hhhh+hhh;
+        System.out.println(result);
+    }
+}
